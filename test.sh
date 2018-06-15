b33fa532a5300b3183d30a64668630c5c78b2184
Horacio G. de Oro <hgdeoro@gmail.com>
2018-06-15 15:29:59 -0300

    Migrate tests to pytest

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-export PYTHONPATH=.
-
-python -m tcpnetlock.test $*
--- tests/test_tcpnetlock.py
+++ tests/test_tcpnetlock.py
@@ -2,29 +2,106 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `tcpnetlock` package."""
+import multiprocessing
+import time
+import uuid
 
 import pytest
-
 from click.testing import CliRunner
 
-from tcpnetlock import tcpnetlock
 from tcpnetlock import cli
+from tcpnetlock.client import LockClient
+from tcpnetlock.server import LockServer
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+def _wait_for_server():
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    Busy-waits until the test server is responding
+    """
+    client = LockClient()
+    for _ in range(10):
+        try:
+            client.connect()
+            break
+        except ConnectionRefusedError:
+            time.sleep(0.1)
+    client.ping()
+    client.close()
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.fixture(scope="module")
+def lock_server():
+    """
+    Fixture, returns the server process running a LockServer ready to use
+    """
+    def start_server():
+        server = LockServer("localhost", 9999)
+        server.serve_forever()
+
+    process = multiprocessing.Process(target=start_server)
+    process.daemon = True
+    process.start()
+    _wait_for_server()  # wait until server is ready
+
+    yield process
+
+    client = LockClient()
+    client.connect()
+    client.server_shutdown()
+    client.close()
+
+    process.join(1)
+    process.terminate()
+
+
+def test_server_is_alive(lock_server):
+    """Test the test server is actually running"""
+    assert lock_server.is_alive()
+    client = LockClient()
+    client.connect()
+    client.ping()
+
+
+def test_connect_and_get_lock_works(lock_server):
+    """Test that a lock can be acquired"""
+    client = LockClient()
+    client.connect()
+    acquired = client.lock(uuid.uuid4().hex)
+    assert acquired
+    client.close()
+
+
+def test_get_two_different_lock(lock_server):
+    """Test that two different locks can be acquired"""
+    client = LockClient()
+    client.connect()
+    acquired = client.lock(uuid.uuid4().hex)
+    assert acquired
+    client.close()
+
+    client2 = LockClient()
+    client2.connect()
+    acquired = client2.lock(uuid.uuid4().hex)
+    assert acquired
+    client2.close()
+
+
+def test_lock_twice(lock_server):
+    """Test that a locks can NOT be acquired twice"""
+    name = uuid.uuid4().hex
+
+    client1 = LockClient()
+    client1.connect()
+    acquired = client1.lock(name)
+    assert acquired
+
+    client2 = LockClient()
+    client2.connect()
+    acquired = client2.lock(name)
+    assert not acquired
+
+    client1.close()
+    client2.close()
 
 
 def test_command_line_interface():
