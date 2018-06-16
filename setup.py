d3ac49706f97a9e7463946fb3ff8bcc9dd4a1c2f
Horacio G. de Oro <hgdeoro@gmail.com>
2018-06-15 22:38:50 -0300

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hgdeoro/tcpnetlock',
-    version='0.0.2',
+    version='0.0.3',
     zip_safe=False,
 )
