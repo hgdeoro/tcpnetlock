d953760e0880989fd3bf1055c70d62c7e6497ee3
Horacio G. de Oro <hgdeoro@gmail.com>
2018-06-17 00:00:24 -0300

    Move class to module level

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,47 +7,49 @@ from tcpnetlock.client import LockClient
 from tcpnetlock.server import LockServer
 
 
+class TestServerThread(threading.Thread):
+    def __init__(self, initial_port=7654):
+        super().__init__(daemon=True)
+        server = None
+        for port in range(initial_port, initial_port + 1000):
+            try:
+                server = LockServer("localhost", port)
+            except OSError as err:
+                print("err.errno: {}".format(err.errno))
+        assert server, "Could not bind server"
+        self.server = server
+        self.port = port
+
+    def wait_for_server(self):
+        """Busy-waits until the test server is responding"""
+        connected = False
+        lock_client = LockClient("localhost", self.port)
+        for _ in range(50):
+            try:
+                lock_client.connect()
+                connected = True
+                break
+            except ConnectionRefusedError:
+                time.sleep(0.1)
+        assert connected, "Couldn't connect to test server after many tries"
+        lock_client.ping()
+        lock_client.close()
+
+    def get_client(self, **kwargs):
+        """Returns a client instance to connect to test server"""
+        host = kwargs.pop('host', 'localhost')
+        port = kwargs.pop('port', self.port)
+        return LockClient(host, port, **kwargs)
+
+    def run(self):
+        self.server.serve_forever()
+
+
 @pytest.fixture(scope="module")
 def lock_server():
     """
     Fixture, returns the server process running a LockServer ready to use
     """
-    class TestServerThread(threading.Thread):
-        def __init__(self, initial_port=7654):
-            super().__init__(daemon=True)
-            server = None
-            for port in range(initial_port, initial_port + 1000):
-                try:
-                    server = LockServer("localhost", port)
-                except OSError as err:
-                    print("err.errno: {}".format(err.errno))
-            assert server, "Could not bind server"
-            self.server = server
-            self.port = port
-
-        def wait_for_server(self):
-            """Busy-waits until the test server is responding"""
-            connected = False
-            lock_client = LockClient("localhost", self.port)
-            for _ in range(50):
-                try:
-                    lock_client.connect()
-                    connected = True
-                    break
-                except ConnectionRefusedError:
-                    time.sleep(0.1)
-            assert connected, "Couldn't connect to test server after many tries"
-            lock_client.ping()
-            lock_client.close()
-
-        def get_client(self, **kwargs):
-            """Returns a client instance to connect to test server"""
-            host = kwargs.pop('host', 'localhost')
-            port = kwargs.pop('port', self.port)
-            return LockClient(host, port, **kwargs)
-
-        def run(self):
-            self.server.serve_forever()
 
     server_thread = TestServerThread()
     server_thread.start()
